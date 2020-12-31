0278d4cd9f85384f813583f9d518522c06e72967
Olly Chadwick <olly@homeflow.co.uk>
2020-12-31 17:31:50 +0000

    Update users controller to handle new user submissions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,28 @@
 class UsersController < ApplicationController
+  before_action :split_user_name, only: :create
+
   def new
-    respond_to do |format|
-      format.html
-      format.js
-    end
+    @user = User.new
+  end
+
+  def create
+    User.new(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :first_name, :last_name)
+  end
+
+  def split_user_name
+    full_name = params.dig(:user, :full_name)
+    return unless full_name
+
+    last_name = full_name.split.last
+    first_name = full_name.delete_suffix(last_name).strip
+
+    params["user"].delete(:full_name)
+    params["user"]["first_name"], params["user"]["last_name"] = first_name, last_name
   end
 end
