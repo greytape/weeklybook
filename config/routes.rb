04e7281d26c6c91c9c54730eb0b315c075e959b0
Olly Chadwick <olly@homeflow.co.uk>
2021-01-03 14:16:02 +0000

    Add log-in page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   root 'main#index'
 
   get 'main/index'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
   resources :users
 end
