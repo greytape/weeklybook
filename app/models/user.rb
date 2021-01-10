8bd02a5fdc57f22f4e44604b274b8ef4ffef556c
Olly Chadwick <olly@homeflow.co.uk>
2021-01-10 15:53:08 +0000

    Create GroupsUser model, migrations and associations
    We need to define a relationship between groups and users to display it on the user page.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :groups_users
+  has_many :groups, through: :groups_users
+
   def full_name
-    "#{first_name} #{last_name}"  
+    "#{first_name} #{last_name}"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_10_150244) do
+ActiveRecord::Schema.define(version: 2021_01_10_153124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2021_01_10_150244) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "groups_users", force: :cascade do |t|
+    t.bigint "group_id"
+    t.bigint "user_id"
+    t.index ["group_id"], name: "index_groups_users_on_group_id"
+    t.index ["user_id"], name: "index_groups_users_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +37,6 @@ ActiveRecord::Schema.define(version: 2021_01_10_150244) do
     t.string "password_digest"
   end
 
+  add_foreign_key "groups_users", "groups"
+  add_foreign_key "groups_users", "users"
 end
