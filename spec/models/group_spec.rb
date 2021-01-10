786f8d497769089f3daab898797b7bddcb5c0950
Olly Chadwick <olly@homeflow.co.uk>
2021-01-10 15:23:25 +0000

    Add required rspec config files and write first basic test

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,7 @@
-require 'rails_helper'
+
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    expect(Group.new(name: 'group_name')).to be_valid
+  end
 end
